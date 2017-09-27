11d758096c9db704b491e98a77f424e72fa145fa
MustafaAmish <bllizzard8585@googlemail.com>
2017-09-27 20:05:45 +0300

    Add 3 more

---
+++ DataTypesAndVariablesLab/08.RefactorVolumeOfPyramid/RefactorVolumeOfPyramid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.RefactorVolumeOfPyramid
+{
+    class RefactorVolumeOfPyramid
+    {
+        static void Main(string[] args)
+        {
+            //double dul, sh, V = 0;
+            double length = double.Parse(Console.ReadLine());
+           double width = double.Parse(Console.ReadLine());
+           double heigth = double.Parse(Console.ReadLine());
+            double volume = (length * width * heigth)/3 ;
+            Console.WriteLine("Length: Width: Height: Pyramid Volume: {0:F2}", volume);
+
+        }
+    }
+}
---
+++ DataTypesAndVariablesLab/09.RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nNumber = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= nNumber; i++)
+            {
+                int courentNumber = i;
+                int sum = 0;
+                while (courentNumber>0)
+                {
+                    sum += courentNumber % 10;
+                    courentNumber = courentNumber / 10;
+                }
+                bool checkSpecialNumbers = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}",i,checkSpecialNumbers);
+            }
+        }
+    }
+}
